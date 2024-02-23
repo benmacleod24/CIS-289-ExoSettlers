fd265f4c88158cf1f48c36b45c114950583203c9
Ben Macleod <benstreasurebox@gmail.com>
2024-02-23 09:20:03 -0500

    feat: session class and session manager

---
+++ ExoSettlers/Assets/Scripts/Classes/Session.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Classes
+{
+    internal class Session
+    {
+        private string sessionId;
+        private DateTime createdAt;
+        private DateTime updatedAt;
+
+        public Entity playerData;
+
+        public Session()
+        {
+            sessionId = System.Guid.NewGuid().ToString();
+            createdAt = new DateTime();
+            updatedAt = new DateTime();
+        }
+
+        public Session(string sessionId)
+        {
+            this.sessionId = sessionId;
+            // Collect the session via id, from session manager.
+        }
+
+        public void saveSession()
+        {
+            string serializedSessionData = JsonUtility.ToJson(this);
+            // Do some sort of saving with the session data here.
+        }
+    }
+}
---
+++ ExoSettlers/Assets/Scripts/Managers/SessionManager.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Scripts.Classes;
+using Assets.Scripts.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Managers
+{
+    internal class SessionManager
+    {
+        static public List<Session> GetSessions()
+        {
+            return new List<Session>();
+        }
+
+        static public Session GetSession(int id)
+        {
+            return new Session();
+        }
+    }
+}
