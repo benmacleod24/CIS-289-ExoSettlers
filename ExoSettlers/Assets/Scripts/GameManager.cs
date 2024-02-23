04f3fec0014e952193c63c970311c3918ce80efe
Ben Macleod <benstreasurebox@gmail.com>
2024-02-23 09:19:39 -0500

    feat: game manager class w/ session property

---
+++ ExoSettlers/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using Assets.Scripts.Classes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Stores the active session of the game.
+    Session activeSession;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
